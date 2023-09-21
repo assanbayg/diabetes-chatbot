91ef15c6e867d8babcceb030c09cf178dd556e54
Gauhar Assanbay <asanbayg@gmail.com>
2023-09-22 05:00:39 +0600

    Add flask, send requests

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 "# diabetes-chatbot" 
 # # Install package
 pip install "unstructured[all-docs]"
-pip install pinecone-client openai tiktoken langchain
+pip install pinecone-client openai tiktoken langchain flask aiohttp
+
+# # Fix list
+For some reason I get correct GET request but cannot send answer correctly
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from flask import Flask, request, jsonify
 import os
 import pinecone
 from dotenv import load_dotenv
@@ -5,39 +6,52 @@ from langchain.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
-from langchain.chains.question_answering import load_qa_chain
+import asyncio
+import aiohttp
 
 load_dotenv()
 
+app = Flask(__name__)
+
 def load_docs():
-  directory = './resources'
-  loader = DirectoryLoader(directory)
-  docs = loader.load()
-  return docs
+    directory = './resources'
+    loader = DirectoryLoader(directory)
+    docs = loader.load()
+    return docs
 
 def split_docs(docs, chunk_size=1000, chunk_overlap=20):
-  text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
-  splitter_docs = text_splitter.split_documents(docs)
-  return splitter_docs
-
-def get_similiar_docs(query, index, k=2, score=False):
-  if score:
-    similar_docs = index.similarity_search_with_score(query, k=k)
-  else:
-    similar_docs = index.similarity_search(query, k=k)
-  return similar_docs
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    splitter_docs = text_splitter.split_documents(docs)
+    return splitter_docs
 
 docs = split_docs(load_docs())
-print(len(docs))
 embeddings = OpenAIEmbeddings(model='gpt-3.5-turbo', openai_api_key=os.getenv('OPENAI_API_KEY'))
-query_result = embeddings.embed_query('Hello World')
-len(query_result)
 pinecone.init(
-  api_key=os.getenv('PINECONE_API_KEY'),
-  environment=os.getenv('PINECONE_API_ENV'),)
+    api_key=os.getenv('PINECONE_API_KEY'),
+    environment=os.getenv('PINECONE_API_ENV'),
+)
 index_name = 'langchain1'
 index = Pinecone.from_documents(documents=docs, embedding=embeddings, index_name=index_name)
 
-query = 'What is Diabetes?'
-docs = index.similarity_search(query)
-print(docs[0].page_content)
+async def perform_similarity_search(query):
+    async with aiohttp.ClientSession() as session:
+        result = await index.similarity_search(query, session=session)
+    return result
+
+@app.route('/query', methods=['GET'])
+async def handle_query():
+    query = request.args.get('query')
+    
+    if query:
+        result = await perform_similarity_search(query)
+        
+        if result:
+            first_result = result[0]
+            return jsonify({"result": first_result.page_content})
+        else:
+            return jsonify({"error": "No results found"})
+    else:
+        return jsonify({"error": "No query provided"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
