6729b8d874652ca2bb3b380ff3a9aeae96ec252a
Gauhar Assanbay <asanbayg@gmail.com>
2023-09-27 09:10:45 +0600

    Search from existing indexes

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def load_docs():
     docs = loader.load()
     return docs
 
-def split_docs(docs, chunk_size=1000, chunk_overlap=20):
+def split_docs(docs, chunk_size=500, chunk_overlap=20):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     splitter_docs = text_splitter.split_documents(docs)
     return splitter_docs
@@ -35,23 +35,24 @@ index = Pinecone.from_documents(documents=docs, embedding=embeddings, index_name
 
 async def perform_similarity_search(query):
     async with aiohttp.ClientSession() as session:
-        result = await index.similarity_search(query, session=session)
+        result = await index.similarity_search(query)
     return result
 
-@app.route('/query', methods=['GET'])
+@app.route('/query', methods=['POST'])
 async def handle_query():
-    query = request.args.get('query')
-    
-    if query:
-        result = await perform_similarity_search(query)
-        
-        if result:
-            first_result = result[0]
-            return jsonify({"result": first_result.page_content})
-        else:
-            return jsonify({"error": "No results found"})
-    else:
-        return jsonify({"error": "No query provided"})
+    try: 
+      data = request.get_json()
+      query = data.get('query')
+      print(query)
+      result = await perform_similarity_search(query=query)
+      print(result)
+      if result:
+          return jsonify({'result': result[0].page_content})
+      else:
+          return jsonify({'result': 'nope'})
+    except Exception as e:  
+      print(e)
+      return jsonify({'error': 'No query provided'})
 
 if __name__ == '__main__':
     app.run(debug=True)
